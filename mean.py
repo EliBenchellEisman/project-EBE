10d55474840fd1b1f8c108a5513c0df7780b7c86
Eli Benchell Eisman <tomokokomiyama@host-57.subnet-110.med.umich.edu>
2014-07-20 10:11:55 -0400

    handle numbers in a file

--- mean.py
+++ mean.py
@@ -1,9 +1,10 @@
+# print the mean of the numbers in the file
 import sys
 
 sum = 0
 n = 0
 # sum inpute values
-for num in sys.stdin:
+for num in open('data.txt'):
     sum += float(num)
     n += 1
 print sum / n
